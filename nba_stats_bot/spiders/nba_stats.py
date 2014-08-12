d4cbc329a9e29f12914538eb6ab2dfbf2f8dc1fa
Louis Tiao <louistiao@gmail.com>
2014-08-12 19:05:29 +1000

    added start urls

--- nba_stats_bot/spiders/nba_stats.py
+++ nba_stats_bot/spiders/nba_stats.py
@@ -1,13 +1,18 @@
 # -*- coding: utf-8 -*-
-import scrapy
+from scrapy import Spider, FormRequest
+import json
 
-
-class NbaStatsSpider(scrapy.Spider):
+class NBAStatsSpider(Spider):
     name = "nba-stats"
     allowed_domains = ["stats.nba.com"]
-    start_urls = (
-        'http://www.stats.nba.com/',
-    )
+
+    def start_requests(self):
+    	yield FormRequest('http://stats.nba.com/stats/commonallplayers',
+			method='GET', formdata={'LeagueID': '00', 'Season': '2013-14', 'IsOnlyCurrentSeason': '0'})
 
     def parse(self, response):
-        pass
+        data = json.loads(response.body_as_unicode())
+        for row in data[u'resultSets'][0][u'rowSet']:
+        	player_id = str(row[0])
+        	yield FormRequest('http://stats.nba.com/stats/commonplayerinfo/',
+        		method='GET', formdata={'PlayerID': player_id})
